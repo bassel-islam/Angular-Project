d5d65577dbb332e17e6169daf777eb8c5472a3d8
bassel <bassel.islam@gmail.com>
2026-03-20 19:52:10 +0200

    cli and components.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from "./pages/app-home/home.component";
+import { About } from "./about/about";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home, About],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
